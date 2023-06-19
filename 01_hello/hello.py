f27cea44e270cfac9d2bae25599ee95d3307ba1b
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2023-06-19 15:42:28 +0100

    Chapter 1 - How to write and test a Python program
    - Changed end of line sequence to UL

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,9 +1,3 @@
-# Tiny Python Projects
-# by Ken Youens-Clark
-
-# CHAPTER 1: How to write and test a Python program
-
-# CODE WRITTEN BY: Steven Partlow
-# DATE: 19/06/2023
-
-print("Hello, World!") # Print "Hello, World" to the console
+#! /user/bin/env python3
+# Say hello
+print("Hello, World!")
