c6b83a984fbf9cfa06529bcea4d38c62ab86cde4
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2023-06-19 13:38:01 +0100

    Chapter 1 - How to write and test a Python program
    - Created 'hello.py'
    - Wrote "Hello World" program
    - Commented code
    - Setup pytest for testing

---
+++ 01_hello/hello.py
@@ -0,0 +1,9 @@
+# Tiny Python Projects
+# by Ken Youens-Clark
+
+# CHAPTER 1: How to write and test a Python program
+
+# CODE WRITTEN BY: Steven Partlow
+# DATE: 19/06/2023
+
+print("Hello, World!") # Print "Hello, World" to the console
