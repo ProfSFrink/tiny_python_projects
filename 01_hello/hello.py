5b2ec5c1326d489604d9dd3acb6bf1f98e680e16
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2023-06-20 13:20:19 +0100

    Chapter 1 - How to write and test a Python program
    - Add optional argument 'name'
    - Add default value for 'name' if none provided
    - Add usage message
    - Update code comments

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+
+# Tiny Python Projects
+# by Ken Youens-Clark
+
+# CHAPTER 1: How to write and test a Python program
+
+# AUTHOR: Steven Partlow
+# DATE STARTED: 19/06/202
+# DATE FINISHED: 
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 # Purpose: Say hello
 
-print('Hello, World!') # Print the below text to the console
+import argparse # Import the argparse module in to the program
+
+parseer = argparse.ArgumentParser(description='Say hello') # Setup our program to accept arguments, the description is our help message
+
+# Make the first argument optional using either -n or --name
+# If no argument is provided the name will default to 'World'
+# # metavar=name is the name of the optional argument that will appear in the usage message
+parseer.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
+
+args = parseer.parse_args() # Take an arguments passed into the program parse them 
+
+print('Hello, ' + args.name + '!') # Concatentate the name argument to this string and output to the console
